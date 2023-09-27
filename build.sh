9609a34222881dcb33d963fb85e90f954496ef15
Kasyfil <mksyfla@gmail.com>
2023-09-27 18:24:07 +0700

    reset

--- build.sh
+++
@@ -1,8 +0,0 @@
-# membuat docker image dengan nama  image berdasarkan format github container registry
-docker build -t ghcr.io/mksyfla/shipping-service:latest .
-
-# login ke dalam github container registry
-echo $GCR_CREDENTIALS | docker login ghcr.io -u mksyfla --password-stdin
-
-# melakukan push image ke github container registry
-docker push ghcr.io/mksyfla/shipping-service:latest
